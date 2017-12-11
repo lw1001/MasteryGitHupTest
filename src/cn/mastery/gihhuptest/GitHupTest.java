64976b99b371b143fdee9081373abfcabb4cfc15
liwei <815457877@qq.com>
2017-12-11 10:06:32 +0800

    dsad

--- src/cn/mastery/gihhuptest/GitHupTest.java
+++ src/cn/mastery/gihhuptest/GitHupTest.java
@@ -4,7 +4,7 @@ public class GitHupTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World!");s
+		System.out.println("Hello World!");
 	}
 
 }
